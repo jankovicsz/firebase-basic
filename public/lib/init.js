3ee16f219497d51bbf8bd0d7c044a4a1f6bc1499
Jankovics Zoltan <jankovicsz@gmail.com>
2021-02-06 17:38:05 +0100

    Add firebase files to starwars app

---
+++ public/lib/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyCaCjldvdjrrIB0eo6pqaUv7T_VhJKUqpg",
+  "appId": "1:373156359641:web:e4c4f19123ef79520abdc8",
+  "authDomain": "starwars-finder.firebaseapp.com",
+  "databaseURL": "",
+  "measurementId": "G-MT8L9Q4E8C",
+  "messagingSenderId": "373156359641",
+  "projectId": "starwars-finder",
+  "storageBucket": "starwars-finder.appspot.com"
+});
